fb67cffed015ced4866dd3f4d130de3fa4771c8c
Evin Grano <evin.grano@gmail.com>
2021-02-20 10:45:48 -0500

    [INIT] more setup

---
+++ index.js
@@ -0,0 +1,15 @@
+"use strict";
+/**
+ * index.js - main program/startup code for CLT
+ * module index.js
+ * @file index.js
+ * @author egrano
+ * @copyright Copyright ©2021, Evin Grano & Associates. All rights reserved.
+ */
+const MODULE_NAME = "main.js";
+var _bmtn = global.BMT_NAMESPACE || {};
+
+//set up our virtual environment vars
+require('dotenv').config();
+
+console.log("I'm here");
